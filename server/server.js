c839c41e126613d52cf74af6d78059392f6dec98
lambert <lb.tran648@gmail.com>
2017-07-21 13:33:07 -0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+
+// make path look pretty
+const path = require('path');
+const publicPath = path.join(__dirname,'../public');
+const port = process.env.PORT || 3000;
+//setup server
+const express=require('express');
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port,() =>{
+   console.log(`server is up on ${port}`);
+})
